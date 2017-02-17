aa34b66ea096f5a7bc04a31fbc69bece681b17e7
paquettemichael <michael@biomasscontrols.com>
2017-02-17 14:56:15 -0500

    fixed sweeping problem

---
+++ SensieTopLevel.m
@@ -0,0 +1,24 @@
+clear all;
+addpath(genpath('../drivers'));
+
+FREQ_MIN = 700e6;
+kHz = 1000; % Hz
+Tx_INT = 4; % Minutes
+
+while (0 == 0)
+    % create sweeper object (could also use constructor)
+    sweeper = spectrumSweeper;
+    sweeper.freq_min = FREQ_MIN;
+    
+    % get spectrum
+    spectrum = sweeper.getSpectrum();
+    % scale to 8 bit integer
+    [spectrum_q , scaling_factor] = sweeper.scaleToInteger (spectrum, 8); 
+    
+    % wait until next Tx syncronization time
+    while ~(mod(minute(datetime('now')), Tx_INT) == 0 && round(second(datetime('now'))) == 0)
+    end
+    %transmitter = AgletTransmitter;
+    
+    %transmitter.transmit(spectrum_q);
+end
