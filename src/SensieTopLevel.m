61a70a1f579959effb60456da796560c736ac939
Michael Paquette <mpaquette@wpi.edu>
2017-02-17 14:10:56 -0500

    sync

--- src/SensieTopLevel.m
+++ src/SensieTopLevel.m
@@ -1,17 +1,25 @@
 clear all;
 addpath(genpath('../drivers'));
 
-i = 0
-while (i == 0)
-    i = i + 1;
+FREQ_MIN = 1900e6;
+kHz = 1000; % Hz
+Tx_INT = 4; % Minutes
+
+while (0 == 0)
     % create sweeper object (could also use constructor)
     sweeper = spectrumSweeper;
-    sweeper.freq_min = 1900e6;
+    sweeper.freq_min = FREQ_MIN;
     
     % get spectrum
     spectrum = sweeper.getSpectrum();
+    % scale to 8 bit integer
     [spectrum_q , scaling_factor] = sweeper.scaleToInteger (spectrum, 8);
     
+    
+    % wait until next Tx syncronization time
+    while ~(mod(minute(datetime('now')), Tx_INT) == 0 && round(second(datetime('now'))) == 0)
+    end
+
     %transmitter = AgletTransmitter;
     
     %transmitter.transmit(spectrum_q);
