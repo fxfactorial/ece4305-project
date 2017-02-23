1e2574371808a7c80578fd4cae754ef3cfa9d819
clfs <clfs@users.noreply.github.com>
2017-02-23 16:45:06 -0500

    Completed visualization code.

---
+++ demo/visualize.m
@@ -0,0 +1,36 @@
+function [dataArray] = visualize(data, dataArray)
+% Takes in data and visualizes it in an updating waterfall plot.
+
+%% Grab global state
+% Before this function is called, dataArray has to be preallocated as an
+% empty array. In the main script, set it to 'zeros(20, 120)'
+
+%% Validate input
+assert(isequal(size(data),[1 120]));
+assert(isequal(size(dataArray), [20 120]));
+
+%% Define constants
+fMin = 1; % 1GHz
+fStep = 0.005; % 5MHz = 0.005GHz
+fMax = 1.6 - fStep; % just less then 1.6GHz to get 120 samples.
+
+numEventsToPlot = 20; % How many sensing events to plot in one graph?
+
+XFreqVector = fMin:fStep:fMax; % X-axis of waterfall
+YEventVector = 1:numEventsToPlot; % Y-axis of waterfall
+
+%% Shift and store new data
+dataArray = circshift(dataArray, 1); % Shift all rows down
+dataArray(1,:) = data; % First row is now new data
+
+%% Plot
+surf(XFreqVector, YEventVector, dataArray);
+title('PlutoSDR Spectrum Waterfall')
+xlabel('Frequency (GHz)')
+ylabel('Measurement #')
+zlabel('Power (dB)')
+ylim([1 20])
+colormap jet
+colorbar
+
+end
