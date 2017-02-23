6d2b48c7a720d19257d5753744592aa7baa8eea0
clfs <clfs@users.noreply.github.com>
2017-02-23 16:44:40 -0500

    Updated tests to include visualization.

--- demo/tests.m
+++ demo/tests.m
@@ -1,7 +1,7 @@
 %% frames2data.m, data2frames.m
 
 % Define constants
-dataTX = 1:1:120;
+dataTX = 1:120;
 scaling = 50;
 
 % Convert data to frames, then TX->RX
@@ -12,3 +12,21 @@ dataRX = frames2data(frames)*scaling;
 assert(all(abs(dataTX-dataRX) < 1e-3));
 disp('frame2data.m and data2frames.m passed tests!');
 
+%% visualize.m
+
+% Define constants
+dataArray = zeros(20,120);
+
+% Plot
+for i = 1:20
+    pause(0.5);
+    
+    % Random data with three peaks
+    data        = zeros(1,120)  + 5*rand(1,120);
+    data(20:35) = 30*ones(1,16) + 5*rand(1,16);
+    data(65:80) = 20*ones(1,16) + 5*rand(1,16);
+    data(80:95) = 10*ones(1,16) + 5*rand(1,16);
+    
+    dataArray = visualize(data,dataArray);
+end
+disp('visualize.m passed tests!');
