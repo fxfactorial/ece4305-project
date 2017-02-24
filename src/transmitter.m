d4fd0c7666f4edc985b9544f3e4e375d11d8b16a
paquettemichael <michael@biomasscontrols.com>
2017-02-23 22:28:51 -0500

    added Disps

--- src/transmitter.m
+++ src/transmitter.m
@@ -1,34 +1,38 @@
 %take in 5x256bits convert to transmits of the pluto 
-function [finished] = transmitter(dataBin)
+function [out] = transmitter(dataBin)
 %% Set up SDR
     data_len = length(dataBin);
     assert(data_len > 0, 'Transmit() was passed empty data');
     
     % initalize SDR
-    sdr = initSdr(2^20, 30.72e6, 916e6, 2e6, 5e6, 15, 'fast-attack', 'transmit');
+    disp('Configure SDR to transmit...');
+    sdr = initSdr(2^20, 30.72e6, 916e6, 2e6, 5e6, 15, 'manual', 'transceive');
     
     numFrames = size(dataBin,1);
-    
 %% Create Waveforms from the data
+    disp('Create waveforms from data');
     dataWave = [];
+    disp('Applying Tx Filter...');
+    disp('Shaping Tx data with Hilbert...');
     for i = 1 : numFrames
         frameWave = bin2waveform(dataBin(i,:), sdr.in_ch_size, sdr.rx_sample_rate);
         dataWave = [dataWave; frameWave];
     end
-
+out = [];
+%pause
 %% Transmit Data
     dataRec = [];
-    pause
+    
     for frame = 1:numFrames % should output 1-5 
         % Transceive with SDR
         transmitFrame = dataWave(frame,:);
-        sdr.transmit(transmitFrame);
+        o = sdr.transceive(transmitFrame);
         
         % Info
+        out = [out o];
         s = strcat({'Frame '}, int2str(frame), {' of '}, int2str(numFrames));
         disp(s)
     end
-
     clear sdr;
 
     finished = 1;
